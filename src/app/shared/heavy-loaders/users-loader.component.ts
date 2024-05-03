f72f9792c0889e18773ce3de0cf9c2733a523925
Edson Sánchez <edsonjsf1990@gmail.com>
2024-05-03 17:00:49 -0400

    feat: generate all components

---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-loader',
+  standalone: true,
+  imports: [],
+  template: ` <h1>user-loader works</h1> `,
+})
+export class UserLoaderComponent {}
