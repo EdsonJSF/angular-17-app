3a02c2ed7bb6c8d0069a96413dcecb54aa02a3a3
Edson Sánchez <edsonjsf1990@gmail.com>
2024-05-07 16:01:08 -0400

    feat: dinamic menu

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { routes } from '../../dashboard/dashboard.routes';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sidemenu.component.html',
   styles: ``,
 })
-export class SidemenuComponent {}
+export class SidemenuComponent {
+  public menuItems = routes
+    .map((routes) => routes.children ?? [])
+    .flat()
+    .filter(
+      (route) =>
+        route && route.path && route.path !== '**' && !route.path.includes(':')
+    );
+}
