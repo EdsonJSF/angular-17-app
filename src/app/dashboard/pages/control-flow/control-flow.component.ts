63a4e3631e949817575a3eaa8cd176e2b02a814b
Edson Sánchez <edsonjsf1990@gmail.com>
2024-05-08 12:39:14 -0400

    feat: control fow (if)

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
-  selector: "app-control-flow",
+  selector: 'app-control-flow',
   standalone: true,
   imports: [],
   templateUrl: './control-flow.component.html',
   styles: ``,
 })
-export class ControlFlowComponent {}
+export class ControlFlowComponent {
+  public showContent = signal(false);
+
+  public toggleContent() {
+    this.showContent.update((value) => !value);
+  }
+}
