055fa3205c722b832fbc821243d942d55b22b926
Edson Sánchez <edsonjsf1990@gmail.com>
2024-05-08 14:47:58 -0400

    feat: control fow (switch)

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,13 +1,18 @@
 import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+type Grade = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | '';
 
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './control-flow.component.html',
   styles: ``,
 })
 export class ControlFlowComponent {
+  gradeForm = new FormControl<Grade>('');
+
   public showContent = signal(false);
 
   public toggleContent() {
