7008c6a92d1444604f53b40d1accdee30931a0fe
Edson Sánchez <edsonjsf1990@gmail.com>
2024-05-15 13:56:24 -0400

    feat: implement user service

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { UsersService } from '@services/users.service';
+import { TitleComponent } from '@share/title/title.component';
 
 @Component({
-  selector: "app-users",
+  selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, TitleComponent],
   templateUrl: './users.component.html',
   styles: ``,
 })
-export class UsersComponent {}
+export class UsersComponent {
+  #userService = inject(UsersService);
+
+  users = this.#userService.users;
+  loading = this.#userService.loading;
+}
